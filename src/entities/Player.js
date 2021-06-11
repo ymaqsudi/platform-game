d56c785bd1381bf6f0c7a356370447d6acca6db7
Yaseen Maqsudi <yaseenmaqsudi@gmail.com>
2021-06-11 13:42:27 -0400

    player movement

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -16,6 +16,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 		this.gravity = 500;
 		this.playerSpeed = 200;
 		this.cursors = this.scene.input.keyboard.createCursorKeys();
+		this.previousSpeed;
 
 		this.body.setGravityY(this.gravity);
 		this.setCollideWorldBounds(true);
@@ -28,10 +29,14 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 	update() {
 		const { left, right } = this.cursors;
 
-		if (left.isDown) {
+		if (left.isDown && right.isDown) {
+			this.setVelocityX(-this.previousSpeed);
+		} else if (left.isDown) {
 			this.setVelocityX(-this.playerSpeed);
+			this.previousSpeed = this.body.velocity.x;
 		} else if (right.isDown) {
 			this.setVelocityX(this.playerSpeed);
+			this.previousSpeed = this.body.velocity.x;
 		} else {
 			this.setVelocityX(0);
 		}
